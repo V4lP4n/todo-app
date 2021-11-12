dfd82fec91bc7398f28a875bd6090018bfbc21b0
V4lP4n <sepure@protonmail.com>
2021-11-12 06:01:25 +0300

    some cosmetic stuff

--- static/main.js
+++ static/main.js
@@ -4,48 +4,53 @@ let host = "http://localhost"
 let list = "/api/list"
 
 
-
-async function get_list(){
-let url = host + list;
-let list_data
-let response = await fetch(url);
-if (response.ok){
-    list_data = await response.json();
-    
-
-} else{
-    console.error(response.status)
+//returns full list of taskbars via json
+async function get_json_list(){
+    let url = host + list;
+    let list_data
+    let response = await fetch(url);
+    if (response.ok){
+        list_data = await response.json();
+    } else{
+        console.error(response.status)
+    }
+    return list_data
 }
-return list_data
-}
-
-function prep_row(num, data, status){
- let tr = document.createElement('tr')
- let id_td = document.createElement('td')
- let data_td = document.createElement('td')
- let status_td = document.createElement('td')
- 
- tr.className="todo_tr "+status
- id_td.className="todo_td"
- data_td.className="todo_td"
-
- 
- id_td.innerHTML=num
- data_td.innerHTML+= data
-
-tr.appendChild(id_td)
+//takes task info and returns html row for it
+    function prep_row(num, data, status){
+    let tr = document.createElement('tr')
+    let data_td = document.createElement('td')    
+    let actions_td = document.createElement('td') 
+    let actions = document.createElement('img')
+    actions.src="/images/gear.svg"
+    actions.className="button"+num
+    tr.className="todo_tr "+status
+    data_td.className="todo_td"    
+    data_td.innerHTML+= data
+    actions_td.appendChild(actions)
     tr.appendChild(data_td)
-return tr
+    tr.appendChild(actions_td)
+
+    menu = document.querySelector("#actions")
+    
+    actions.onclick=function(){
+        menu.className="visible"
+    }
+        
+    return tr
 }
 
+//adding table with taskbars in html
 async function load_table(){
+    let app = document.querySelector('#app')
+    let table = app.querySelector('table')
+    let tbody = table.querySelector('tbody')
+    let data = await get_json_list()
 
-    let tbody = document.querySelector("#tbody")
-
-    let data = await get_list()
     for ( let i=0; i<data.length; i++){
     let row = prep_row(data[i].id, data[i].data, data[i].status)
     tbody.appendChild(row)
+
     }
 }
 load_table()
