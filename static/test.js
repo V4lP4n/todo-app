6e324ef98e08c7395540be5046fb95df89c06304
V4lP4n <sepure@protonmail.com>
2021-11-21 11:41:40 +0300

    tmp

--- static/test.js
+++ static/test.js
@@ -1,9 +1,5 @@
 import {TB} from './js/tables.js'
 
-<<<<<<< HEAD
-let app = document.querySelector('#app')
-=======
 let app = document.querySelector('.app')
->>>>>>> develop
 let tb1 = new TB('qew')
 app.appendChild(tb1.generate())
